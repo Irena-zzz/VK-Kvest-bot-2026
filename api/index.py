61c9f63367dea31f1a0ae9cda1bf9fcc627b3f8e
Irena-zzz <balakireva2312@yandex.com>
2026-04-29 14:23:20 +0300

    index.py

--- api/index.py
+++ api/index.py
@@ -1,12 +1,10 @@
+import json
 import os
 import requests
-import hmac
-import hashlib
 
-# 🔐 ВСТАВЬ СВОИ ДАННЫЕ НИЖЕ (между кавычками)
-CONFIRMATION_CODE = "05b07636"      # ← сюда строку подтверждения
-GROUP_TOKEN = "vk1.a.CkPgupAiYsZNaeUMoopB1SzXeShDo2gndVLxtiWSvJ1LZ5-X3aXw_-YyrCyCtAD6aPwzJMmkZXDFn4lMK0gT3n7-l1bedKDmdjrSe-9OZGbwfGToCYM39VOdsZN3xB0E7IiH3p084Yp2flFn308fZ5N2xFhomSvF97k8DxxXqwkbsUPnTKQSv6DvosUqaWwkCqUMIQTDFkg8BCumwpCyoQ"            # ← сюда токен сообщества
-SECRET_KEY = "23i12r19i72n"           # ← сюда секретный ключ
+CONFIRMATION_CODE = os.environ.get('05b07636', 'test')
+GROUP_TOKEN = os.environ.get('vk1.a.CkPgupAiYsZNaeUMoopB1SzXeShDo2gndVLxtiWSvJ1LZ5-X3aXw_-YyrCyCtAD6aPwzJMmkZXDFn4lMK0gT3n7-l1bedKDmdjrSe-9OZGbwfGToCYM39VOdsZN3xB0E7IiH3p084Yp2flFn308fZ5N2xFhomSvF97k8DxxXqwkbsUPnTKQSv6DvosUqaWwkCqUMIQTDFkg8BCumwpCyoQ')
+SECRET_KEY = os.environ.get('23i12r19i72n')
 
 VK_API = "https://api.vk.com/method"
 
@@ -15,17 +13,31 @@ def send_message(peer_id, text):
         "peer_id": peer_id,
         "message": text,
         "random_id": 0,
-        "access_token": G… # 2. Сообщение
+        "access_token": GROUP_TOKEN,
+        "v": "5.199"
+    })
+
+def handler(event, context):
+    try:
+        body = json.loads(event.get('body', '{}'))
+    except:
+        return {'statusCode': 400, 'body': 'Error'}
+
+    event_type = body.get('type')
+
+    if event_type == 'confirmation':
+        return {'statusCode': 200, 'body': CONFIRMATION_CODE}
+
     if event_type == 'message_new':
         msg = body['object']['message']
         peer_id = msg['peer_id']
         text = msg.get('text', '').lower()
 
         if 'привет' in text:
-            send_message(peer_id, 'Привет! Я работаю на Yandex Cloud ☁️')
+            send_message(peer_id, 'Привет! Я работаю на Bothost!')
         elif 'как дела' in text:
             send_message(peer_id, 'Всё отлично!')
         else:
-            send_message(peer_id, 'Я пока умею только здороваться. Напиши "привет".')
+            send_message(peer_id, 'Напиши "привет"')
 
     return {'statusCode': 200, 'body': 'ok'}
